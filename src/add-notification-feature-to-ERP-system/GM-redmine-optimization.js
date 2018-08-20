dccce36069770f6a4a43b8b2d253296986884c83
Snger <sjs.stef@gmail.com>
2018-08-20 15:59:42 +0800

    upgrade GM-redmine-optimization.js
    1. 新增配置项，可以自由配置需要添加的按钮（提供三个参数：链接、文本、按钮插入位置）；
    2. 自动识别 bug 列表中的前端童鞋，并高亮；

--- src/add-notification-feature-to-ERP-system/GM-redmine-optimization.js
+++ src/add-notification-feature-to-ERP-system/GM-redmine-optimization.js
@@ -1,17 +1,68 @@
 // ==UserScript==
 // @name         RedMine优化
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
 // @author       Snger
-// @match        http://redmine.cnstrong.cn/*
+// @match        {{domain}}/*
 // @grant        none
+// @description  todo: 请用 RedMine 路径url 全局替换 {{domain}}
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     // Your code here...
-    var insertHtml = '<div style="float:left;letter-spacing: 1.2px;"><a href="/issues?cf_33=me&amp;set_filter=1&amp;sort=priority%3Adesc%2Cupdated_on%3Adesc">指派给我的问题</a></div>';
-    $( insertHtml ).insertBefore( "#account" );
+    var addBtnsToPage = btns => {
+        btns.map(btn => {
+            var htmlLeftSide;
+            var htmlRightSide;
+            if(btn.insertType === "before"){
+                htmlLeftSide = '<div style="float:left;letter-spacing: 1.2px;">';
+                htmlRightSide = '</div>';
+            } else {
+                htmlLeftSide = '<li>';
+                htmlRightSide = '</li>';
+            }
+            var htmlTemp = `
+                ${htmlLeftSide}
+                    <a href="${btn.href}">${btn.text}</a>
+                ${htmlRightSide}`;
+            if(btn.insertType === "before"){
+                $( htmlTemp ).insertBefore( "#account" );
+            } else {
+                $( htmlTemp ).appendTo( "#top-menu > ul" );
+            }
+        })
+    };
+    var btnsConfig = [{
+        text: "指派给我的问题",
+        href: "/issues?cf_33=me&amp;set_filter=1&amp;sort=priority%3Adesc%2Cupdated_on%3Adesc",
+        insertType: "before"
+    }, {
+        text: "项目-大版本 bug",
+        href: "{{domain}}/projects/big-ver/issues",
+        insertType: "after"
+    }, {
+        text: "项目-作业 bug",
+        href: "{{domain}}/projects/zuoye/issues",
+        insertType: "after"
+    }, {
+        text: "项目-学情 bug",
+        href: "{{domain}}/projects/learnanaly/issues",
+        insertType: "after"
+    }];
+    addBtnsToPage(btnsConfig);
+    if($('table.issues')){
+        var issuesForUserList = $('table.issues .user>a.active');
+        var frontEndTeam = []; //名字不需要空格；
+        // var frontEndTeamStr = frontEndTeam.join('');
+        issuesForUserList.map(key => {
+            var userName = issuesForUserList[key].innerText.trim().replace(/\s/g,'');
+            var isFETeamMember = frontEndTeam.indexOf(userName) > -1 ? true : false;
+            if(isFETeamMember){
+                issuesForUserList[key].parentNode.parentNode.style.backgroundColor='#FFFF84';
+            }
+        });
+    }
 })();
